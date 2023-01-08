17bb3be3b14c4cb0f37bc504008f929a7c45b1a3
NguyenNgocThach2001 <nnthach2001@gmail.com>
2023-01-09 02:05:27 +0700

    Done

--- detector.py
+++ detector.py
@@ -3,17 +3,18 @@ import time
 
 import numpy
 import pose_estimation_class as pm
+import random
 import mediapipe as mp
 import time
 
-DRAW_GLOWING_LIGHT_TIME = 2
-GLOWING_TIME = 2
+DRAW_GLOWING_LIGHT_TIME = 3
+GLOWING_TIME = 1
 TIME_TO_ADD_GLOWING_LIGHT_POINT = 0.01
 TIME_TO_ADD_GLOWING_LIGHT_SUM = 0
 GLOWING_LIGHT_TIME_COUNT = 0
 TURN_ON_OFF_LIGHT = 1
 ANIMATION_INDEX = 0
-NUMBER_OF_ANIMATION = 1
+NUMBER_OF_ANIMATION = 4
 detector = pm.PoseDetector()
 
 vid = cv2.VideoCapture(0)
@@ -26,7 +27,7 @@ sword = cv2.transpose(sword)
 sword = numpy.flip(sword,axis = 0)
 cv2.imshow("sword.jpg", sword)
 
-hand_light_queue = [[0,0,0,0]]
+hand_light_queue = []
 
 def add_glowing_light_point_to_queue(x, y, radius, time):
     hand_light_queue.append([x,y,radius, time])
@@ -62,17 +63,16 @@ while(True):
         rIGHT[1] = rIGHT[1] - int(sword.shape[1]*2)
         RIGHT[0] = RIGHT[0] - int(sword.shape[0]/5)
         RIGHT[1] = RIGHT[1] - int(sword.shape[1] * 2)
-        result_with_sword = img.copy()
-        # if(RIGHT[0] < img.shape[1] and RIGHT[1] < img.shape[0] and RIGHT[0] > 0 and RIGHT[1] > 0):
-        #     result_with_sword[RIGHT[1]:min(img.shape[0],RIGHT[1] + sword.shape[0]),
-        #                     RIGHT[0]:min(img.shape[1],RIGHT[0] + sword.shape[1])] = sword[
-        #                     0:min(img.shape[0],RIGHT[1] + sword.shape[0]) - RIGHT[1],
-        #                     0:min(img.shape[1],RIGHT[0] + sword.shape[1]) - RIGHT[0]]
-        #     for i in range(RIGHT[1],min(img.shape[0],RIGHT[1] + sword.shape[0])):
-        #         for j in range(RIGHT[0],min(img.shape[1],RIGHT[0] + sword.shape[1])):
-        #             for k in range(3):
-        #                 if(result_with_sword[i,j,k] == 0):
-        #                     result_with_sword[i,j,k] = img[i,j,k]
+        if(RIGHT[0] < img.shape[1] and RIGHT[1] < img.shape[0] and RIGHT[0] > 0 and RIGHT[1] > 0):
+            result_with_sword[RIGHT[1]:min(img.shape[0],RIGHT[1] + sword.shape[0]),
+                            RIGHT[0]:min(img.shape[1],RIGHT[0] + sword.shape[1])] = sword[
+                            0:min(img.shape[0],RIGHT[1] + sword.shape[0]) - RIGHT[1],
+                            0:min(img.shape[1],RIGHT[0] + sword.shape[1]) - RIGHT[0]]
+            for i in range(RIGHT[1],min(img.shape[0],RIGHT[1] + sword.shape[0])):
+                for j in range(RIGHT[0],min(img.shape[1],RIGHT[0] + sword.shape[1])):
+                    for k in range(3):
+                        if(result_with_sword[i,j,k] == 0):
+                            result_with_sword[i,j,k] = img[i,j,k]
         
         #NeonLight
         LEFT = [int((HandPosition[0][0] + HandPosition[2][0]) / 2),int((HandPosition[0][1] + HandPosition[2][1]) / 2)]
@@ -91,18 +91,53 @@ while(True):
 
         if(TIME_TO_ADD_GLOWING_LIGHT_SUM > TIME_TO_ADD_GLOWING_LIGHT_POINT):
             TIME_TO_ADD_GLOWING_LIGHT_SUM = 0
-            add_glowing_light_point_to_queue(rIGHT[0],rIGHT[1], 10, GLOWING_TIME)
-
+            if(ANIMATION_INDEX == 0 or ANIMATION_INDEX == 2):
+                add_glowing_light_point_to_queue(rIGHT[0],rIGHT[1], 10, GLOWING_TIME)
+        result = img.copy()
         if(ANIMATION_INDEX == 0):
             for id in range(len(hand_light_queue)):
-                result_with_sword = cv2.circle(result_with_sword, (hand_light_queue[id][0],hand_light_queue[id][1]), radius = hand_light_queue[id][2], color=(0, 0, 255), thickness=-1)
+                if(id == 0):
+                    hand_light_queue[id][3] -= delta_time * 2
+                    continue
+                num1 = int(random.randrange(255))
+                num2 = int(random.randrange(255))
+                num3 = int(random.randrange(255))
+                print(num1,num2,num3)
+                result_with_sword = cv2.line(result_with_sword, (hand_light_queue[id - 1][0],hand_light_queue[id - 1][1]), (hand_light_queue[id][0],hand_light_queue[id][1]), color=(num1, num2, num3), thickness=hand_light_queue[id][2])
                 hand_light_queue[id][1] =int(hand_light_queue[id][1] + delta_time * 20)
-                hand_light_queue[id][2] =int(hand_light_queue[id][2] + delta_time * 10)
-                hand_light_queue[id][3] -= delta_time
+                hand_light_queue[id][2] = max(1,hand_light_queue[id][2] - int(hand_light_queue[id][2] * 0.2))
+                hand_light_queue[id][3] -= delta_time * 2
             hand_light_queue = [item for item in hand_light_queue if item[3] > 0]
+            result = result_with_sword
         
+        if(ANIMATION_INDEX == 1 or ANIMATION_INDEX == 3):
+            context = str(int(GLOWING_LIGHT_TIME_COUNT) + 1)
+            num1 = int(random.randrange(255))
+            num2 = int(random.randrange(255))
+            num3 = int(random.randrange(255))
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            org = (100, 100)
+            fontScale = 2
+            color = (num1,num2,num3)
+            thickness = 5
+            result = cv2.putText(result_with_sword, context, org, font, 
+                            fontScale, color, thickness, cv2.LINE_AA)
+
+        if(ANIMATION_INDEX == 2):
+            for id in range(len(hand_light_queue)):
+                num1 = int(random.randrange(255))
+                num2 = int(random.randrange(255))
+                num3 = int(random.randrange(255))
+                result_with_sword = cv2.circle(result_with_sword, (int(hand_light_queue[id][0] + num1 / 50 - num2  / 50), int(hand_light_queue[id][1] + num2  / 50 - num3  / 50)), color=(num1, num2, num3), thickness=-1, radius = int(10))
+                hand_light_queue[id][2] =max(1,hand_light_queue[id][2] - int(hand_light_queue[id][2] * 0.2))
+                hand_light_queue[id][3] -=delta_time * 2
+            hand_light_queue = [item for item in hand_light_queue if item[3] > 0]
+            result = result_with_sword
+        
+
+
         cv2.imshow("Sword Hand Position", sword)
         cv2.imshow("Image POSE", img)
-        cv2.imshow("Result", result_with_sword)
+        cv2.imshow("Result", result)
         cv2.waitKey(1)
 
